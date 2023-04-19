b9530f6247fe8ee06cdb632977b8e4196e5c651b
Raphael Kienhöfer <rk085@hdm-stuttgart.de>
2023-04-19 17:04:06 +0200

    feat(backend): add OpenAPI

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableShutdownHooks();
+
+  // Configure OpenAPI
+  const oaConfig = new DocumentBuilder()
+    .setTitle('SwipeFood API')
+    .setDescription('Backend API for the SwipeFood App')
+    .setVersion('1.0')
+    .build();
+  const oaDoc = SwaggerModule.createDocument(app, oaConfig);
+  SwaggerModule.setup('swagger', app, oaDoc);
 
   await app.listen(3000);
 }
