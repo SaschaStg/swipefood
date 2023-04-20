869525b16ddc132245a5d62636a29831d50e00b6
Sascha Schmidt <ss532@hdm-stuttgart.de>
2023-04-20 16:56:36 +0200

    add routing for recipe details, add mock html

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {SwipeComponent} from "./swipe/swipe.component";
+import {RecipeDetailsComponent} from "./recipe-details/recipe-details.component";
 
 const routes: Routes = [
   {
     path: '',
     component: SwipeComponent,
+  },
+  {
+    path: 'information',
+    component: RecipeDetailsComponent,
   }
 ];
 
--- frontend/src/app/recipe-details/recipe-details.component.ts
+++ frontend/src/app/recipe-details/recipe-details.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from "../models/recipe";
+import {RecipeService} from "../services/recipe.service";
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.component.html',
   styleUrls: ['./recipe-details.component.css']
 })
-export class RecipeDetailsComponent {
+export class RecipeDetailsComponent implements OnInit {
 
+  recipe?: Recipe;
+
+  constructor(private recipeService: RecipeService) {
+
+  }
+
+  ngOnInit(): void {
+    this.recipeService.getRecipeData().subscribe(data => {
+      this.recipe = data;
+    });
+  }
 }
