3d8c350e9ad9338d51af8e8e2a35882c8cf38a54
Zalenia <101601575+Zalenia@users.noreply.github.com>
2023-06-15 00:23:27 +0200

    create a rough layout of the site
    Forms and inputs are current not working

--- frontend/src/app/recipe-input/recipe-input.component.ts
+++ frontend/src/app/recipe-input/recipe-input.component.ts
@@ -1,14 +1,34 @@
 import {Component} from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
-import {FormControl} from '@angular/forms';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from "@angular/material/select";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-recipe-input',
   templateUrl: './recipe-input.component.html',
   styleUrls: ['./recipe-input.component.scss'],
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [MatTabsModule, MatSelectModule, MatChipsModule, ReactiveFormsModule, MatButtonModule],
 })
+
 export class RecipeInputComponent {
   recipe = new FormControl('');
+  houres = [
+    {value:0.5, viewValue:'0.5h'},
+    {value:1, viewValue:'1h'},
+    {value:1.5, viewValue:'1.5h'},
+    {value:2, viewValue:'2h'},
+    {value:2.5, viewValue:'0.5h'},
+    {value:3, viewValue:'0.5h'},
+    {value:3.5, viewValue:'0.5h'},
+    {value:4, viewValue:'0.5h'},
+    {value:4.5, viewValue:'0.5h'},
+    {value:5, viewValue:'0.5h'},
+    ]
+ // for testing the values
+  amount = new FormControl('');
+  ingredient = new FormControl('');
+  instructions = new FormControl('');
 }
