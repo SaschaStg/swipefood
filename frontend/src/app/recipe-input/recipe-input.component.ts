e7c7ea1152deff9a16218154d1109d7fa867d402
Zalenia <101601575+Zalenia@users.noreply.github.com>
2023-06-16 21:31:09 +0200

    edit icons and trying things

--- frontend/src/app/recipe-input/recipe-input.component.ts
+++ frontend/src/app/recipe-input/recipe-input.component.ts
@@ -1,34 +1,63 @@
 import {Component} from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
-import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {FormBuilder, FormControl, ReactiveFormsModule} from '@angular/forms';
 import {MatSelectModule} from "@angular/material/select";
 import {MatChipsModule} from "@angular/material/chips";
 import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-recipe-input',
   templateUrl: './recipe-input.component.html',
   styleUrls: ['./recipe-input.component.scss'],
   standalone: true,
-  imports: [MatTabsModule, MatSelectModule, MatChipsModule, ReactiveFormsModule, MatButtonModule],
+  imports: [MatTabsModule, MatSelectModule, MatChipsModule, ReactiveFormsModule, MatButtonModule, MatCardModule, MatIconModule],
 })
 
 export class RecipeInputComponent {
-  recipe = new FormControl('');
-  houres = [
-    {value:0.5, viewValue:'0.5h'},
-    {value:1, viewValue:'1h'},
-    {value:1.5, viewValue:'1.5h'},
-    {value:2, viewValue:'2h'},
-    {value:2.5, viewValue:'0.5h'},
-    {value:3, viewValue:'0.5h'},
-    {value:3.5, viewValue:'0.5h'},
-    {value:4, viewValue:'0.5h'},
-    {value:4.5, viewValue:'0.5h'},
-    {value:5, viewValue:'0.5h'},
-    ]
- // for testing the values
-  amount = new FormControl('');
-  ingredient = new FormControl('');
-  instructions = new FormControl('');
+  constructor(private fb: FormBuilder) {
+  }
+
+  recipeForm = this.fb.group({
+    id: [''],
+    title: [''],
+    readyInMinutes: [''],
+    readyInHoures: [''], //must be multiplied by 60 and added to the Minutes
+    servings: [''],
+    image: [''],
+    imageType: [''],
+    summary: [''],
+    instructions: [''],
+
+    categories: this.fb.group({
+      vegetarian: [''],
+      vegan: [''],
+      glutenFree: [''],
+      dairyFree: [''],
+    }),
+
+    extendedIngredients: this.fb.group({
+      id: [''],
+      name: [''],
+      amount: [''],
+      unit: ['']
+    })
+});
+
+recipe = new FormControl('');
+
+// for testing the values
+amount = new FormControl('');
+ingredient = new FormControl('');
+instructions = new FormControl('');
+
+updateRecipe()
+{
+  return 0;
+}
+addIngredient(){
+  return 0;
+}
+
 }
