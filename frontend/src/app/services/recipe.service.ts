67b9b30154b8900188b2d529b5e23eada5b7b33e
Zalenia <101601575+Zalenia@users.noreply.github.com>
2023-06-20 12:59:14 +0200

    removed unused stuff

--- frontend/src/app/services/recipe.service.ts
+++ frontend/src/app/services/recipe.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {catchError, Observable} from "rxjs";
+import {Observable} from "rxjs";
 import {Recipe} from "../models/recipe";
 import {HttpClient} from "@angular/common/http";
 
