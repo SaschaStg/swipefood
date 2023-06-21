21fa3714abae68c20d1d6e374b8cee118c902798
Hein Nikola <nh096@hdm-stuttgart.de>
2023-06-21 19:56:11 +0200

    remove imports, fixed button

--- frontend/src/app/welcome/welcome.component.ts
+++ frontend/src/app/welcome/welcome.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import {Router} from "@angular/router";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {AuthService} from "../auth/auth.service";
-import {AuthResult} from "../auth/auth-result";
-import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-welcome',
