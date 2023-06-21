aac79b84c6f9ee5ffe8be3cb03e32d271dbc4b95
Hein Nikola <nh096@hdm-stuttgart.de>
2023-06-21 10:10:55 +0200

    Resolve "Allow changing display name in frontend"

---
+++ frontend/src/app/services/updateUser.ts
@@ -0,0 +1,7 @@
+export interface UpdateUser {
+  displayName?: string;
+  vegan: boolean;
+  vegetarian: boolean;
+  glutenFree: boolean,
+  dairyFree: boolean
+}
--- frontend/src/app/settings/settings.component.ts
+++ frontend/src/app/settings/settings.component.ts
@@ -2,9 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {UserService} from "../services/user.service";
 import {User} from "../services/user";
 import {FormControl, FormGroup} from '@angular/forms';
-import {Diet} from "../services/diet";
 import {MatChipSelectionChange} from "@angular/material/chips";
-import {SnackBarService} from "../services/snackbar.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {UpdateUser} from "../services/updateUser";
 
 @Component({
   selector: 'app-settings',
@@ -12,12 +12,12 @@ import {SnackBarService} from "../services/snackbar.service";
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
-  user: User = {dairyFree: false, displayName: "", glutenFree: false, id: 0, vegan: false, vegetarian: false};
-  updatedUser: User = {dairyFree: false, displayName: "", glutenFree: false, id: 0, vegan: false, vegetarian: false};
+  user: User = {id: 0, displayName: "", dairyFree: false, glutenFree: false, vegan: false, vegetarian: false};
+  updatedUser: User = {id: 0, displayName: "", dairyFree: false, glutenFree: false, vegan: false, vegetarian: false};
 
   UserData = new FormGroup({
-    displayName: new FormControl('fooBar'),
-    password: new FormControl('fooBar'),
+    displayName: new FormControl('', {nonNullable: true}),
+    password: new FormControl('fooBar', {nonNullable: true}),
     vegetarian: new FormControl(),
     vegan: new FormControl(),
     glutenFree: new FormControl(),
@@ -26,56 +26,49 @@ export class SettingsComponent implements OnInit {
 
   constructor(
     private userService: UserService,
-    private snackBarService: SnackBarService,
+    private snackBar: MatSnackBar,
   ) {
 
   }
 
   ngOnInit(): void {
-    this.userService.getUserName().subscribe({
-      next: (data) => {
-        this.user = data;
-        this.UserData.patchValue({
-          displayName: data.displayName + " !WIP! (not yet implemented)",
-          vegetarian: data.vegetarian,
-          vegan: data.vegan,
-          glutenFree: data.glutenFree,
-          dairyFree: data.dairyFree,
-        });
-      },
-      error: (err) => {
-        this.snackBarService.openSnackBar("can´t load user data!", "warn");
-        console.log(err)
-      }
+    this.userService.getUserName().subscribe(data => {
+      this.user = data;
+      this.UserData.patchValue({
+        displayName: data.displayName,
+        vegetarian: data.vegetarian,
+        vegan: data.vegan,
+        glutenFree: data.glutenFree,
+        dairyFree: data.dairyFree,
+      });
     });
   }
 
   updateUserData() {
-    if (this.updatedUser.vegetarian == this.user.vegetarian &&
+    if (
+      this.UserData.get('displayName')?.value == this.user.displayName &&
+      this.updatedUser.vegetarian == this.user.vegetarian &&
       this.updatedUser.vegan == this.user.vegan &&
       this.updatedUser.glutenFree == this.user.glutenFree &&
-      this.updatedUser.dairyFree == this.user.dairyFree) {
-      this.snackBarService.openSnackBar("No changes detected", "warn");
+      this.updatedUser.dairyFree == this.user.dairyFree
+    ) {
+      this.openSnackBar("No changes detected", "warn");
       return;
     }
-    const userDiet: Diet = {
+    const updateUser: UpdateUser = {
+      displayName: this.UserData.get('displayName')?.value,
       vegan: this.updatedUser.vegan,
       vegetarian: this.updatedUser.vegetarian,
       glutenFree: this.updatedUser.glutenFree,
       dairyFree: this.updatedUser.dairyFree
     }
-    this.userService.patchUserDiet(userDiet).subscribe({
-      next: (data) => {
-        this.user.vegetarian = data.vegetarian;
-        this.user.vegan = data.vegan;
-        this.user.glutenFree = data.glutenFree;
-        this.user.dairyFree = data.dairyFree;
-        this.snackBarService.openSnackBar("Saved Changes!");
-      },
-      error: (err) => {
-        this.snackBarService.openSnackBar("can´t update user data!", "warn");
-        console.log(err)
-      }
+    this.userService.patchUserDiet(updateUser).subscribe(data => {
+      this.user.displayName = data.displayName;
+      this.user.vegetarian = data.vegetarian;
+      this.user.vegan = data.vegan;
+      this.user.glutenFree = data.glutenFree;
+      this.user.dairyFree = data.dairyFree;
+      this.openSnackBar("Saved Changes!");
     });
   }
 
@@ -97,4 +90,15 @@ export class SettingsComponent implements OnInit {
     }
   }
 
+  openSnackBar(message: string, style?: string) {
+    const snackbarStyle = style == "warn" ? "snackbarWarn" : "snackbarPrimary";
+    const sbRef = this.snackBar.open(
+      message, 'Close', {
+        duration: 4000,
+        panelClass: [snackbarStyle],
+      })
+    sbRef.onAction().subscribe(() => {
+      sbRef.dismiss();
+    });
+  }
 }
