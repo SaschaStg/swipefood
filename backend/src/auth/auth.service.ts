d39ae5d2feb00160c5e5a3979b31e70f1a0f64ab
Raphael Kienhöfer <rk085@hdm-stuttgart.de>
2023-06-21 19:44:16 +0200

    feat(backend): add endpoint to change username and password

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { UserLogin } from './user-login.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -6,10 +6,18 @@ import * as argon2 from 'argon2';
 import { User } from '../users/user.entity';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
-import { RegisterUserDto } from './dto';
+import { CredentialUpdateDto, RegisterUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
+  private readonly argonOptions: argon2.Options & { raw?: false } = {
+    // Values from https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#argon2id
+    type: argon2.argon2id,
+    memoryCost: 12288,
+    timeCost: 3,
+    parallelism: 1,
+  };
+
   constructor(
     @InjectRepository(UserLogin)
     private loginRepo: Repository<UserLogin>,
@@ -64,14 +72,10 @@ export class AuthService {
     // Create user login
     const login = new UserLogin();
     login.username = registerUserDto.username;
-    login.password = await argon2.hash(registerUserDto.password, {
-      raw: false,
-      // Values from https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#argon2id
-      type: argon2.argon2id,
-      memoryCost: 12288,
-      timeCost: 3,
-      parallelism: 1,
-    });
+    login.password = await argon2.hash(
+      registerUserDto.password,
+      this.argonOptions,
+    );
     login.user = user;
     await this.loginRepo.save(login);
 
@@ -81,5 +85,42 @@ export class AuthService {
   async isUsernameAvailable(username: string): Promise<boolean> {
     const logins = await this.loginRepo.findBy({ username });
     return logins.length === 0;
+  }
+
+  async updateCredentials(
+    credentialUpdate: CredentialUpdateDto,
+    user: User,
+  ): Promise<{ access_token: string }> {
+    const userLogin = await this.loginRepo.findOneOrFail({
+      where: { user },
+      relations: { user: true },
+    });
+
+    // Check if the current password is correct
+    if (
+      !(await argon2.verify(
+        userLogin.password,
+        credentialUpdate.currentPassword,
+      ))
+    ) {
+      throw new UnauthorizedException('Wrong password');
+    }
+
+    // Update username
+    if (credentialUpdate.newUsername) {
+      userLogin.username = credentialUpdate.newUsername;
+    }
+
+    // Update password
+    if (credentialUpdate.newPassword) {
+      userLogin.password = await argon2.hash(
+        credentialUpdate.newPassword,
+        this.argonOptions,
+      );
+    }
+
+    await this.loginRepo.save(userLogin);
+
+    return this.login(userLogin.user);
   }
 }
