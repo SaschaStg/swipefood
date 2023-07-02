3f78441bdf29c8b334711940c776ef8035852c7c
Raphael Kienhöfer <rk085@hdm-stuttgart.de>
2023-07-02 16:18:21 +0200

    feat(backend): enable global validation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,13 @@ async function bootstrap() {
     .build();
   const oaDoc = SwaggerModule.createDocument(app, oaConfig);
   SwaggerModule.setup('swagger', app, oaDoc);
+
+  // Add global input validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
   await app.listen(3000);
 }
