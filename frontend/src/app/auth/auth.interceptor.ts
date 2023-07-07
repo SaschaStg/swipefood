fb9b05924924da83efeb1c7cb00d56210e800a6c
Raphael Kienhöfer <rk085@hdm-stuttgart.de>
2023-07-08 00:55:31 +0200

    refactor(frontend): change to cookie auth

--- frontend/src/app/auth/auth.interceptor.ts
+++ frontend/src/app/auth/auth.interceptor.ts
@@ -21,21 +21,14 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     // only intercept api requests
     if (request.url.startsWith('api/')) {
-      // only add the authorization header if the user is logged in
-      if (this.authService.isLoggedIn()) {
-        // clone the request to add the new header
-        const authenticatedRequest = request.clone({
-          setHeaders: {authorization: `Bearer ${this.authService.token}`}
-        });
-        return next.handle(authenticatedRequest).pipe(
-          tap(event => {
-            if (event.type === HttpEventType.Response && event.status === HttpStatusCode.Unauthorized) {
-              // if the token is not recognized by the server, log the user out
-              this.authService.logout();
-            }
-          }),
-        );
-      }
+      return next.handle(request).pipe(
+        tap(event => {
+          if (event.type === HttpEventType.Response && event.status === HttpStatusCode.Unauthorized) {
+            // if the token is not recognized by the server, log the user out
+            this.authService.logout();
+          }
+        }),
+      );
     }
     return next.handle(request);
   }
